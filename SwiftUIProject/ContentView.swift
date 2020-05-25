f6b86115d0a75eb11049fba00d1475857fd76848
hende <2419036884@qq.com>
2020-05-25 17:58:50 +0800

    初始化项目

---
+++ SwiftUIProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIProject
+//
+//  Created by bailun on 2020/5/25.
+//  Copyright © 2020 hend. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
