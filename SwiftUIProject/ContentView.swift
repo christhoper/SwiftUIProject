be17fc1fca9f9a6c2cd14015b0a2b2bd751cc661
hende <2419036884@qq.com>
2020-05-26 11:28:02 +0800

    Day1

--- SwiftUIProject/ContentView.swift
+++ SwiftUIProject/ContentView.swift
@@ -9,8 +9,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    let students: [String] = ["张三","李四","王五"]
+    @State private var selectedIndex: Int = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Picker("选择学生", selection: $selectedIndex) {
+                ForEach(0..<self.students.count) {
+                    Text(self.students[$0])
+                }
+            }
+            Text("您的选择是：\(students[selectedIndex])")
+        }
     }
 }
 
