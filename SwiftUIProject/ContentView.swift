51425a7f6b9f924f6f75267ab01271b10188d5ec
hende <2419036884@qq.com>
2020-05-27 16:39:17 +0800

    Day3

--- SwiftUIProject/ContentView.swift
+++ SwiftUIProject/ContentView.swift
@@ -9,66 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students: [String] = ["张三","李四","王五"]
-    @State private var selectedIndex: Int = 0
-    /// 订单金额
-    @State private var checkoutAmount: String = ""
-    @State private var numberOfPeople: Int = 2
-    /// 小费
-    @State private var tipPercentage: Int = 2
-    let tipPercentages: [Int] = [10, 15, 20, 25, 0]
-    /// 一个订单下来，平均每人需要付的钱
-    var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
-        let tipSelected = Double(tipPercentages[tipPercentage])
-        let orderAmout = Double(checkoutAmount) ?? 0
-        
-        let tipValue = orderAmout / 100 * tipSelected
-        let grandTotal = orderAmout + tipValue
-        let amoutPerPerson = grandTotal / peopleCount
-        
-        return amoutPerPerson
-    }
-    
-    
+    @State private var showingAlert = false
     var body: some View {
-//        VStack {
-//            Picker("选择学生", selection: $selectedIndex) {
-//                ForEach(0..<self.students.count) {
-//                    Text(self.students[$0])
-//                }
-//            }
-//            Text("您的选择是：\(students[selectedIndex])")
-//        }
-        NavigationView {
-            Form {
-                Section {
-                    TextField("请输入现金",text: $checkoutAmount)
-                        .keyboardType(.decimalPad)
+        /// 默认是center
+        VStack() {
+            Text("我来了")
+                .background(Color.red)
+            Text("你还是在下面吧")
+                .background(Color.green)
+//            Spacer()
+            //            LinearGradient(gradient: Gradient(colors: [.blue, .gray]), startPoint: .top, endPoint: .bottom)
+//            RadialGradient(gradient: Gradient(colors: [.blue, .gray]), center: .center, startRadius: 20, endRadius: 200)
+//            AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+            
+            
+            Button(action: {
+                print("响应事件")
+                self.showingAlert = true
+            }) {
+                HStack(alignment: .center, spacing: 50, content: {
                     
-                    Picker("人数", selection: $numberOfPeople) {
-                        ForEach(2..<100) {
-                            Text("\($0) People")
-                        }
-                    }
-                }
-                
-                Section {
-                    Text("现金：\(totalPerPerson)")
-                }
-                
-                Section(header: Text("小费所占百分比")) {
-                    Picker("小费", selection: $tipPercentage) {
-                        ForEach(0..<tipPercentages.count) {
-                            Text("\(self.tipPercentages[$0])%")
-                        }
-                    }
-                .pickerStyle(SegmentedPickerStyle())
-                }
+                    Image(systemName: "pencil")
+                    Text("点击我")
+                })
             }
-            .navigationBarTitle(Text("Swift"))
+            .alert(isPresented: $showingAlert) { () -> Alert in
+                Alert(title: Text("温馨提示"), message: Text("终将成为过去的"), dismissButton: .default(Text("是的")))
+            }
+            
         }
-        
+        .background(Color.yellow)
     }
 }
 
